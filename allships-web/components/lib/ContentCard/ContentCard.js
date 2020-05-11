1085927447854072f6f58b24aa24a4bb4057709a
Peter Laxalt <pete@laxaltandmciver.co>
2020-05-11 16:03:53 -0400

    🎨 prevent rerender of cards on state updates

--- allships-web/components/lib/ContentCard/ContentCard.js
+++ allships-web/components/lib/ContentCard/ContentCard.js
@@ -15,46 +15,102 @@
 //////////////////////////////////////////////////////////////////////
 
 // Core
-import React from "react";
+import React, { Component } from "react";
 
 // Styles
 import { GlobalStyles } from "./styles.scss";
 
 // Utils
 import LazyImage from "../../../utils/lazyImage";
+import { render } from "react-dom";
 
 // Begin Component
 //////////////////////////////////////////////////////////////////////
 
-export const ContentCard = ({ isLink, data }) => {
-  const Content = ({ data }) => {
-    let { Attachments, Name } = data;
-
-    return (
-      <div className="content-card-inner">
-        <LazyImage src={Attachments[0].url} alt={Name} />
-      </div>
-    );
-  };
-
-  if (!isLink) {
-    return (
-      <div className="content-card">
-        <Content data={data} />
-      </div>
-    );
-  } else {
-    return (
-      <a
-        href={data.Link}
-        className="content-card __isLink"
-        target="_blank"
-        rel="nofollow noreferrer"
-      >
-        <Content data={data} />
-      </a>
-    );
+export class ContentCard extends Component {
+  constructor(props) {
+    super(props);
   }
-};
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.data === nextProps.data) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  render() {
+    let { isLink, data } = this.props;
+
+    const Content = ({ data }) => {
+      let { Attachments, Name } = data;
+
+      return (
+        <div className="content-card-inner">
+          {Attachments && Name ? (
+            <LazyImage src={Attachments[0].url} alt={Name} />
+          ) : (
+            <div>No Image</div>
+          )}
+        </div>
+      );
+    };
+
+    if (!isLink) {
+      return (
+        <div className="content-card">
+          <Content data={data} />
+        </div>
+      );
+    } else {
+      return (
+        <a
+          href={data.Link}
+          className="content-card __isLink"
+          target="_blank"
+          rel="nofollow noreferrer"
+        >
+          <Content data={data} />
+        </a>
+      );
+    }
+  }
+}
+
+// export const ContentCard = ({ isLink, data }) => {
+//   const Content = ({ data }) => {
+//     let { Attachments, Name } = data;
+
+//     return (
+//       <div className="content-card-inner">
+//         {Attachments && Name ? (
+//           <LazyImage src={Attachments[0].url} alt={Name} />
+//         ) : (
+//           <div>No Image</div>
+//         )}
+//       </div>
+//     );
+//   };
+
+//   if (!isLink) {
+//     return (
+//       <div className="content-card">
+//         <Content data={data} />
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <a
+//         href={data.Link}
+//         className="content-card __isLink"
+//         target="_blank"
+//         rel="nofollow noreferrer"
+//       >
+//         <Content data={data} />
+//       </a>
+//     );
+//   }
+// };
 
 export const ContentCardGlobalStyles = () => <GlobalStyles />;
