fe88e373d6e2002b8c6eb554b6eb7a4330d98400
Peter Laxalt <pete@laxaltandmciver.co>
2020-08-18 11:43:57 -0400

    🎨 convert layout to functional component and migrate mothership to it's own class

--- web/lib/gtag.ts
+++ web/lib/gtag.ts
@@ -16,12 +16,16 @@ type GoogleTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined") {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, params }: GoogleTagEvent) => {
-  window.gtag("event", action, params);
+  if (typeof window !== "undefined") {
+    window.gtag("event", action, params);
+  }
 };
